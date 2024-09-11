1cfc237473144d7b84df4ba32822520fc9bd8752
DanielaMendozaA <97774155+DanielaMendozaA@users.noreply.github.com>
2024-09-11 18:47:19 -0500

    console.log deleted

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -17,7 +17,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     private readonly configService: ConfigService,
   ) {
     const secret = configService.get<string>('JWT_SECRET');
-    console.log('secret', secret);
     
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -26,12 +25,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
   async validate(payload: JwtPayload): Promise<User> {
-    console.log('payload' ,payload);
-    
   
-    const user = await this.usersService.findUserById(payload.id)
-    console.log('user', user);
-    
+    const user = await this.usersService.findUserById(payload.id)    
   
     if (!user) {
       throw new UnauthorizedException('Token not valid');
